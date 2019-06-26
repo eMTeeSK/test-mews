82fa5063d8c72c889a1a2f292aae3f3f8eeb9be8
Martin Charvát <martin.charvat@praguelabs.com>
2019-06-27 00:19:02 +0200

    Implementiation of test assignment

---
+++ ExchangeRateUpdater/Interfaces/ICnbParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater.Interfaces
+{
+    public interface ICnbParser
+    {
+        Dictionary<string, decimal> Parse(string txtContent);
+    }
+}
